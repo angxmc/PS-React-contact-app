1fee5fd3f6178703c57ba9b60b207cbee7ec0ef2
angxmc <2017xchen@gmail.com>
2023-08-03 14:56:31 -0400

    deleteContact updated

--- src/components/ContactItem/index.js
+++ src/components/ContactItem/index.js
@@ -1,12 +1,22 @@
+import { useContext } from "react";
+import { ContactsContext } from "../../context/contactsContext";
 //we want to export this component and export in the contact list
+
 function ContactItem(props) {
-  const { name, email, phone } = props.contact;
+
+  const contactCtx = useContext(ContactsContext);
+  const dispatch = contactCtx;
+
+  const { name, email, phone,id } = props.contact;
   return (
     <div>
       {/* display some data for each contact */}
       <h4>{name}</h4>
       <h5>{email}</h5>
       <h5>{phone}</h5>
+      <button onClick={() => dispatch({ type: "delete_contact", payload: id })}>
+        Delete
+      </button>
     </div>
   );
 }
--- src/reducer/contactsReducer.js
+++ src/reducer/contactsReducer.js
@@ -1,19 +1,27 @@
 //
 /**
  * @description store all state logic
- * @param {*} state
- * @param {*} action
+ * @param {*} state: current state
+ * @param {*} action: objects with teh type and payload
  * @returns
  */
 export default function contactsReducer(state, action) {
   //going to contain a switch function
   const { type, payload } = action;
+  console.log(payload);
 
   switch (type) {
+    //returning new state with teh data from the API
     case "fetched_data":
       return [...payload];
+
+      //this is triggered from ContactForm
     case "add_contact":
-      break;
+      return [...state, payload];
+      case "delete_contact":
+        const copyState=[...state];
+        const newArr = copyState.filter(item => item.id !== payload)
+        return [...newArr]
     default:
       return [];
   }
